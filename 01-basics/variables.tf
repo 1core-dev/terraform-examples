27310efc7e81dc9a71f5043d33259f92f770af78
1CORE <1core@protonmail.com>
2025-04-09 13:21:47 +0300

    TF variables and output usage

---
+++ 01-basics/variables.tf
@@ -0,0 +1,12 @@
+variable "server_port" {
+  description = "The port that the server will use to handle HTTP requests"
+  type        = number
+  default     = 8080
+
+  validation {
+    condition     = var.server_port > 0 && var.server_port < 65536
+    error_message = "The port number must be between 1-65353"
+  }
+
+  #   sensitive = true
+}
