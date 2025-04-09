3a7a1140f16de8019409ba7c99ad28de126c1fa9
1CORE <1core@protonmail.com>
2025-04-09 14:38:44 +0300

    Added AWS Elastic Load Balancing

--- 01-basics/main.tf
+++ 01-basics/main.tf
@@ -13,6 +13,25 @@ data "aws_subnets" "default" {
   }
 }
 
+resource "aws_instance" "example" {
+  ami           = "ami-04f167a56786e4b09"
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "my-ubuntu"
+  }
+}
+
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-04f167a56786e4b09"
   instance_type   = "t3.micro"
@@ -43,7 +62,10 @@ resource "aws_security_group" "instance" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier  = [data.aws_subnets.default.ids]
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_alb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -52,5 +74,83 @@ resource "aws_autoscaling_group" "example" {
     key                 = "Name"
     value               = "web"
     propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "web"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # By default it just shows a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "web-alb"
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_alb_target_group" "asg" {
+  name     = "web-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.asg.arn
   }
 }
