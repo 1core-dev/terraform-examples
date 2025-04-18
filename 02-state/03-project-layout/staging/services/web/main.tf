f84af46ae5f922b5596348ad5c1275e86a135001
1CORE <1core@protonmail.com>
2025-04-18 14:39:32 +0300

    Rafactoring commit

--- 02-state/03-project-layout/staging/services/web/main.tf
+++ 02-state/03-project-layout/staging/services/web/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.0.0, < 2.0.0"
+  required_version = ">= 1.7.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
@@ -13,38 +13,78 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_launch_configuration" "example" {
-  image_id        = "ami-0fb653ca2d3203ac1"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
+# resource "aws_launch_configuration" "example" {
+#   image_id        = "ami-0fb653ca2d3203ac1"
+#   instance_type   = "t2.micro"
+#   security_groups = [aws_security_group.instance.id]
 
-  # Render the User Data script as a template
-  user_data = templatefile("user-data.sh", {
+#   # Render the User Data script as a template
+#   user_data = templatefile("user-data.sh", {
+#     server_port = var.server_port
+#     db_address  = data.terraform_remote_state.db.outputs.address
+#     db_port     = data.terraform_remote_state.db.outputs.port
+#   })
+
+#   # Required when using a launch configuration with an auto scaling group.
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+
+resource "aws_launch_template" "example" {
+  name_prefix   = "example-"
+  image_id      = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = base64encode(templatefile("user-data.sh", {
     server_port = var.server_port
     db_address  = data.terraform_remote_state.db.outputs.address
     db_port     = data.terraform_remote_state.db.outputs.port
-  })
+  }))
 
-  # Required when using a launch configuration with an auto scaling group.
   lifecycle {
     create_before_destroy = true
   }
 }
 
+# resource "aws_autoscaling_group" "example" {
+#   launch_configuration = aws_launch_configuration.example.name
+#   vpc_zone_identifier  = data.aws_subnets.default.ids
+
+#   target_group_arns = [aws_lb_target_group.asg.arn]
+#   health_check_type = "ELB"
+
+#   min_size = 2
+#   max_size = 10
+
+#   tag {
+#     key                 = "Name"
+#     value               = "terraform-asg-example"
+#     propagate_at_launch = true
+#   }
+# }
+
 resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier  = data.aws_subnets.default.ids
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
+  vpc_zone_identifier = data.aws_subnets.default.ids
 
-  target_group_arns = [aws_lb_target_group.asg.arn]
-  health_check_type = "ELB"
-
-  min_size = 2
-  max_size = 10
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
 
   tag {
     key                 = "Name"
-    value               = "terraform-asg-example"
+    value               = "example-asg"
     propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
