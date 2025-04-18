aa71d8d30ff72cd3abd4d687ba4bee3fc7c573fc
1CORE <1core@protonmail.com>
2025-04-18 12:21:11 +0300

    Add Terraform backend.hcl

---
+++ 02-state/s3-backend-native-locking/backend.hcl
@@ -0,0 +1,5 @@
+bucket         = "gl0bal-un1que-6ucket-tf-sta7e"
+dynamodb_table = "terraform-state"
+region         = "us-east-2"
+encrypt        = true
+use_lockfile = true
--- 02-state/s3-backend-native-locking/backend.tf
+++ 02-state/s3-backend-native-locking/backend.tf
@@ -1,11 +1,9 @@
+// Initialize with S3 backend:
+//    terraform init -backend-config=./backend.hcl
+//
+// This stores the state file in S3 at "global/s3/terraform.tfstate".
 terraform {
   backend "s3" {
-    # Replace this with your bucket name!
-    bucket = "gl0bal-un1que-6ucket-tf-sta7e"
-    key    = "global/s3/terraform.tfstate"
-    region = "us-east-2"
-
-    # S3 backend native locking
-    use_lockfile = true
+    key = "global/s3/terraform.tfstate"
   }
 }
