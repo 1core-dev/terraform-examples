ad8f2181ef1b4c6c75cdc54a7abe4d806d61b75f
1CORE <1core@protonmail.com>
2025-04-24 17:05:26 +0300

    EC2 IAM role

---
+++ 05-managing-secrets/01-ec2-iam-role/variables.tf
@@ -0,0 +1,9 @@
+// -----------------------------------------------------------------------------
+// OPTIONAL PARAMETER
+// These parameters have reasonable defaults.
+// -----------------------------------------------------------------------------
+variable "name" {
+  description = "The name used to namespace all the resources created by this module"
+  type        = string
+  default     = "ec2-iam-role-example"
+}
