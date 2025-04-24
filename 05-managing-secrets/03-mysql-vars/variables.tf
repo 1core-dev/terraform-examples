b1e12e3230a45521a3a0439daecb500ed81e2f8e
1CORE <1core@protonmail.com>
2025-04-24 17:36:02 +0300

    Using env vars as secrets

---
+++ 05-managing-secrets/03-mysql-vars/variables.tf
@@ -0,0 +1,25 @@
+// -----------------------------------------------------------------------------
+// REQUIRED PARAMETER
+// You must provide a value for each of these parameters.
+// -----------------------------------------------------------------------------
+variable "db_username" {
+  description = "The username for the database"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "The password for the database"
+  type        = string
+  sensitive   = true
+}
+
+// -----------------------------------------------------------------------------
+// OPTIONAL PARAMETER
+// These parameters have reasonable defaults.
+// -----------------------------------------------------------------------------
+variable "db_name" {
+  description = "The name to use for the database"
+  type        = string
+  default     = "example"
+}
